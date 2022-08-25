c6350cdfea2c8c2a6816870fc12a6cd71ecd864e
nandakho <anandakhosuri@yahoo.com>
2022-08-25 14:06:15 +0700

    Add stderr for unexpected argument

--- src/App.java
+++ src/App.java
@@ -3,10 +3,12 @@ import net.sf.jasperreports.engine.JasperCompileManager;
 
 public class App {
     public static void main(String[] jrxml) throws Exception {
-        if (jrxml.length == 1 ) {
+        if (jrxml.length == 1) {
             String jasper = FilenameUtils.getFullPath(jrxml[0])+FilenameUtils.getBaseName(jrxml[0])+".jasper";
             JasperCompileManager.compileReportToFile(jrxml[0],jasper);
             System.out.println(jasper+" generated!");
+        } else {
+            System.err.println("Expecting 1 .jrxml's file path as argument, but "+jrxml.length+" argument(s) given!");
         }
     }
 }
