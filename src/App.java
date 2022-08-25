133ff96721fbfebd4d3295daa750a6ccb14afbc0
nandakho <anandakhosuri@yahoo.com>
2022-08-25 13:42:17 +0700

    Fix relative path problem

--- src/App.java
+++ src/App.java
@@ -4,10 +4,9 @@ import net.sf.jasperreports.engine.JasperCompileManager;
 public class App {
     public static void main(String[] jrxml) throws Exception {
         if (jrxml.length == 1 ) {
-            String jasper = FilenameUtils.getPath(jrxml[0])+FilenameUtils.getBaseName(jrxml[0])+".jasper";
-            System.out.println(jrxml[0]);
-            System.out.println(jasper);
+            String jasper = FilenameUtils.getFullPath(jrxml[0])+FilenameUtils.getBaseName(jrxml[0])+".jasper";
             JasperCompileManager.compileReportToFile(jrxml[0],jasper);
+            System.out.println(jasper+" generated!");
         }
     }
 }
